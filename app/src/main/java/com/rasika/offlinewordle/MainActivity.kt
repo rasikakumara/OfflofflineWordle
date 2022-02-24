763e32088cc45e0d8d5c6da10410c5269cf9aa74
rasikakumara <rasika1600@gmail.com>
2022-02-25 01:15:18 +0530

    Initial commit

---
+++ app/src/main/java/com/rasika/offlinewordle/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rasika.offlinewordle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
